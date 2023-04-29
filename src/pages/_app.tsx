bbba6f57b4bd3a3e746a5f1487c661ca548484db
Oscar Sarabia <ferarersunl@hotmail.com>
2023-04-29 11:25:00 -0600

    google ads

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,30 @@
-import "./styles.css";
-import Header from "@/components/NavBar";
-import FooterBar from "@/components/Footer";
-import theme from "../theme";
-import { ChakraProvider } from "@chakra-ui/react";
-import { SessionProvider } from "next-auth/react";
-import type { AppProps } from "next/app";
+import Script from 'next/script';
+import './styles.css';
+import Header from '@/components/NavBar';
+import FooterBar from '@/components/Footer';
+import theme from '../theme';
+import { ChakraProvider } from '@chakra-ui/react';
+import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app';
 
 export default function App({
-    Component,
-    pageProps: { session, ...pageProps },
+  Component,
+  pageProps: { session, ...pageProps },
 }: AppProps) {
-    return (
-        <SessionProvider session={session}>
-            <ChakraProvider theme={theme}>
-                <Header />
-                <Component {...pageProps} />
-                <FooterBar />
-            </ChakraProvider>
-        </SessionProvider>
-    );
+  return (
+    <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7371462935139459"
+        crossOrigin="anonymous"
+      ></Script>
+      <SessionProvider session={session}>
+        <ChakraProvider theme={theme}>
+          <Header />
+          <Component {...pageProps} />
+          <FooterBar />
+        </ChakraProvider>
+      </SessionProvider>
+    </>
+  );
 }
